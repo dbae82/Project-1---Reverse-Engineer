f31ec7058f118f07fe447fb0b9a2f01557b896a6
dbae82 <dbae82@gmail.com>
2021-07-30 22:35:51 -0500

    cleaned up index.ejs and main.scss

--- public/scripts/app.js
+++
@@ -1,4 +0,0 @@
-const flipCardContainer = document.querySelector('.flip-card-container');
-flipCardContainer.addEventListener('click', function() {
-    flipCardContainer.classList.toggle('flip');
-});
