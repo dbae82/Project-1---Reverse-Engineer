87b782b60c415d57dce283f6786c062cf7ebcb84
Username <jwaffensmith@gmail.com>
2021-07-31 15:44:49 -0700

    refactored index route

--- controllers/movie_controllers.js
+++ controllers/movie_controllers.js
@@ -2,15 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 
-const Movie = require('../models/Movie');
+const { Movie } = require('../models/Movie');
 
 /* index route */
-router.get('/', function (req, res, next) {
-     return res.render('./index');
-     if (error) {
-     console.log(error);
-     req.error = error;
-     return next();
+router.get('/', async function (req, res, next) {
+    try {
+        const allMovies = await Movie.find({});
+        const context = {
+            movies: allMovies,
+        };
+    return res.render('./index', context);
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+       return next();
     }
 });
 
