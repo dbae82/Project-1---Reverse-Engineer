a3681ecc84eb3e24c0abc4c1142f2fd16c2293ca
Username <jwaffensmith@gmail.com>
2021-07-31 15:12:32 -0700

    added Movie model

---
+++ models/Movie.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    runtime: {
+        type: Date
+    },
+    genre: {
+        type: String
+
+    },
+    cast: {
+        type: String
+    },
+    image: {
+        type: String
+    }
+},
+{
+    timestamps: true
+});
+
+
+const Movie = mongoose.model('Movie', movieSchema );
+
+module.exports = Movie;
