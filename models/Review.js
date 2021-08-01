cae5bca9a76fba98af869bb79291a83c7623b5ff
dbae82 <dbae82@gmail.com>
2021-08-01 11:02:26 -0500

    adds review and user schema to models

--- models/Review.js
+++ models/Review.js
@@ -2,7 +2,27 @@ const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema(
     {
-        rating: Number
+        rating: {
+            type: Number,
+            default: 0,
+            min: 0,
+            max: 5,
+        },
+        content: {
+            type: String,
+            required: [true, "You must provide a reason for your review score."],
+        },
+        movie: {
+            tyoe: mongoose.Types.ObjectId,
+            ref: 'Movie',
+        },
+        user: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+        }
+    },
+    {
+        timestamps: true
     }
 );
 
