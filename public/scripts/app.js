97611e439adf7f2ea03711230eb4f4141378f156
Username <jwaffensmith@gmail.com>
2021-08-01 15:23:59 -0700

    changed modal background color

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,19 @@
-const modal = 
-document.querySelector('.modal');
+const modal = document.querySelector('.modal');
 
-const btn = 
-document.querySelector('#review-btn')
+const btn = document.querySelector('#review-btn')
 
-const close = 
-document.querySelector('.modal-close')
+const close = document.querySelector('.modal-close')
 
-btn.addEventListener('click',
-               function () {
-modal.style.display = 'block'
-})
+btn.addEventListener('click', function () {
+    modal.style.display = 'block'
+});
 
-close.addEventListener('click',
-                 function () {
-modal.style.display = 'none'
-})
+close.addEventListener('click', function () {
+    modal.style.display = 'none'
+});
 
-window.addEventListener('click',
-                  function (event) {
-if (event.target.className === 
-'modal-background') {
-modal.style.display = 'none'
+window.addEventListener('click', function (event) {
+    if (event.target.className === 'modal-background') {
+    modal.style.display = 'none'
 }
-})
+}); 
