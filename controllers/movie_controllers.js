fcf18691ede180400efce736eb5bd6ea60204ffc
dbae82 <dbae82@gmail.com>
2021-08-02 11:51:55 -0500

    finally got delete working, now working on routing problem

--- controllers/movie_controllers.js
+++ controllers/movie_controllers.js
@@ -50,7 +50,7 @@ router.post('/:id', async function(req, res, next) {
 router.put('/:id', async function(req, res, next) {
     try {
         const updatedReview = await Review.findByIdAndUpdate(
-            reviews._id, 
+            review._id, 
             {
                 $set: req.body,
             },
@@ -68,9 +68,11 @@ router.put('/:id', async function(req, res, next) {
 
 router.delete('/:id', async function(req, res, next) {
     try {
-        const foundReview = await Review.findById(reviews._id);
-        await foundReview.delete();
-        return res.redirect(`/${req.params.id}`);
+        // const foundReview = await Review.findById(reviews.id);
+        // await foundReview.delete();
+        await Review.findByIdAndDelete(req.params.id);
+        console.log(movie.id);
+        return res.send('hello');
     } catch (error) {
         console.log(error);
         req.error = error;
