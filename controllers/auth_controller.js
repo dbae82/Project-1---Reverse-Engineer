6ceada2522016d301b93e4db65a8f9e700e28cd9
dbae82 <dbae82@gmail.com>
2021-08-03 14:28:28 -0500

    feat: added register post route to add to user collections in mongo

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -3,6 +3,31 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../models');
 
-router.get('/')
+// router.get('/register', function (req, res) {
+//     return res.send('register, got it!');
+// });
+
+// router.get('/login', function (req, res) {
+//     return res.send('login, got it!');
+// });
+
+router.post('/register', async function (req, res, next) {
+    try {
+        const foundUser = await User.exists({ 
+            $or: [{ email: req.body.email }, { username: req.body.username }],
+        });
+        if (foundUser) {
+            return res.redirect('/');
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
+        const newUser = await User.create(req.body);
+        return res.redirect('/');
+    } catch (error) {
+        console.log(error);
+        return res.send(error);
+    }
+});
 
 module.exports = router;
