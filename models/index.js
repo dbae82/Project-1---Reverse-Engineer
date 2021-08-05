bc97eedd9539641617a2ced28af00b961402f80a
dbae82 <dbae82@gmail.com>
2021-08-05 12:56:55 -0500

    uncommented out index

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 require("../config/db.connection");
 
-// const mongoose = require('mongoose');
-// mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/badapple');
+const mongoose = require('mongoose');
+
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/badapple');
 
 module.exports = {
     Movie: require("./Movie"),
